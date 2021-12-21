c997fd44f4e8005dfe8c495d54f66a4a05313684
PAVANMAHANKALI <32824156+PAVANMAHANKALI@users.noreply.github.com>
2021-12-21 19:07:46 +0530

    Update HomeController.java

--- src/main/java/com/alpha/HomeController.java
+++ src/main/java/com/alpha/HomeController.java
@@ -29,7 +29,7 @@ public class HomeController {
 	}
 	@GetMapping("/home")
 	public String home() {
-	return "welcome to ANTS project API";	
+	return "welcome to ANTS project API Application to access user";	
 	}
 	
 	@GetMapping("/hello")
